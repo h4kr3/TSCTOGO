bea859968b91d595e0f233b488be352ce9439cfa
hk8031 <hk8031@gmail.com>
2023-03-23 12:23:47 +0530

    objects

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 // let employee : {id:number,name?:string} = {id:1}
-let employee : {id:number,name:string} = {id:1,name:''}
+// let employee : {id:number,name:string} = {id:1,name:''}
+let employee : {readonly id:number,name:string} = {id:1,name:''}
