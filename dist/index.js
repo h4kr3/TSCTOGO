cce230ac68ce556e29919825a90b903bd2b4932b
hk8031 <hk8031@gmail.com>
2023-03-24 10:55:33 +0530

    nul types

--- dist/index.js
+++ dist/index.js
@@ -1,9 +1,56 @@
 "use strict";
-let employee = {
+let array = [32, 134, 413, 143, 143, 413];
+let string = ['wqre', 'fewgrq'];
+let any = [3546321, 'dfsgfh'];
+let str = ['feqw', 'fegeg'];
+string[5] = 'rwgbe';
+let direction = [2431, 'asdfgsf', true];
+let direction1 = [false, 'asdfgsf', true];
+let obj = {
     id: 1,
-    name: 'string',
-    retire: (date) => {
-        console.log(date);
-    }
+    name: 'hari',
+    istrue: false
 };
+obj.istrue = true;
+console.log(obj);
+let car = { model: 2016 };
+car.isPetrol = true;
+console.log(car);
+var CardinalDirection;
+(function (CardinalDirection) {
+    CardinalDirection[CardinalDirection["North"] = 100] = "North";
+    CardinalDirection[CardinalDirection["South"] = 101] = "South";
+    CardinalDirection[CardinalDirection["East"] = 102] = "East";
+    CardinalDirection[CardinalDirection["West"] = 103] = "West";
+})(CardinalDirection || (CardinalDirection = {}));
+console.log(CardinalDirection.North);
+console.log(CardinalDirection.West);
+let direction3 = CardinalDirection.South;
+let car1 = {
+    model: 2000,
+    year: 2014
+};
+let car2 = {
+    model: 3000,
+    year: 2012
+};
+console.log(car1);
+let jay = [2012, 2012, 'benz'];
+console.log(jay);
+let jay1 = { name: 'hari' };
+jay1.year = 35212;
+console.log(jay1);
+function nameServer(incoming) {
+    console.log(typeof (incoming));
+}
+let process;
+let doSome;
+let some = 100;
+function Name(name) {
+    if (name === '')
+        console.log(name.toUpperCase);
+    else
+        console.log('fdgfg');
+}
+Name('eqfwgreth');
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,124 @@
-type Employee = {
-    readonly id : number,
-    name : string,
-    retire : (date : Date) => void
+
+// let set : any = 565768574564
+//     set = 'fewgegnr'
+//     set = true
+
+let array : number[] = [32,134,413,143,143,413]
+let string : string[] = ['wqre','fewgrq']
+let any : any [] = [3546321,'dfsgfh']
+
+let str : readonly string[] = ['feqw','fegeg']
+
+string[5]='rwgbe'
+
+let direction : [number,string,boolean] = [2431,'asdfgsf',true]
+let direction1 : [boolean,string,boolean] = [false,'asdfgsf',true]
+
+// console.log(direction1);
+
+
+
+let obj : {id : number, name : string, istrue : boolean} = {
+    id : 1,
+    name : 'hari',
+    istrue : false
 }
 
-let employee : Employee = {
-    id:1,
-    name : 'string',
-    retire : (date : Date)=>{
-        console.log(date);
-    }
+obj.istrue = true
+
+console.log(obj);
+
+
+let car : {model : number , isPetrol ?: boolean} = {model : 2016}
+
+car.isPetrol = true
+
+console.log(car);
+
+enum CardinalDirection  {
+    North = 100,
+    South,
+    East,
+    West
 }
+
+console.log(CardinalDirection.North);
+console.log(CardinalDirection.West);
+
+let direction3 = CardinalDirection.South
+
+
+
+
+type car = {
+    model : number,
+    year : number
+}
+
+
+let car1 : car = {
+    model : 2000,
+    year : 2014
+}
+let car2 : car = {
+    model : 3000,
+    year : 2012
+}
+
+console.log(car1);
+
+
+
+
+type Sample = [model : number,
+    year : number,
+    name: string]
+    
+
+let jay : Sample = [2012,2012,'benz']
+
+console.log(jay);
+
+
+interface Sample1 { name : string , year ?: number}
+
+let jay1 : Sample1 = {name : 'hari' }
+
+jay1.year = 35212
+
+console.log(jay1);
+
+
+
+function nameServer (incoming : string | number){
+    console.log(typeof(incoming));
+}
+
+
+
+type addition = {
+    add : () => void
+}
+type substraction = {
+    sub : () => void
+}
+
+let process : addition & substraction 
+
+let doSome : {
+    add : () => {},
+    sub : () => {}
+}
+
+type Quantity = 50 | 100
+
+let some : Quantity = 100
+
+
+function Name (name : string | null | undefined | number){
+    if(name === '')
+    console.log(name.toUpperCase)
+    else
+    console.log('fdgfg'); 
+}
+Name('eqfwgreth')
